39c602cdf0e37906513d303124662dfc1a0f1b20
lorna gathumbi <nyawiralornah@gmail.com>
2024-10-16 13:59:23 +0300

    added items

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.getElementById("button").addEventListener('click',()=>{
     let inputValue = document.getElementById('inputName').value
-    
+    let details = document.getElementById("details")
+    details.innerHTML = ""
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
     .then(response=> response.json())
     .then(data => {
@@ -32,5 +33,31 @@ function details(id){
     console.log(id)
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
     .then(res => res.json())
-    .then(detail => console.log(detail))
+    .then(detail =>{
+        let meal = detail.meals[0]
+        console.log(meal)
+        let details = document.getElementById("details")
+        details.innerHTML = ""
+        let detailsDiv = document.createElement("div")
+        let detailsInfo = `
+        <div class="card" style="width: 19rem;">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}">
+            <div class="card-body">
+               <h3 class="card-text">${meal.strMeal}</h3>
+               <h6>Ingredients</h6>
+               <ul>
+                  <li>${meal.strArea}</li>
+                  <li>${meal.strCategory}</li>
+                  <li>${meal.strIngredient1}</li>
+                  <li>${meal.strIngredient2}</li>
+                  <li>${meal.strIngredient3}</li>
+                  <li>${meal.strIngredient4}</li>
+                  <li>${meal.strIngredient5}</li>
+               </ul>
+            </div>
+        </div>
+        `
+        detailsDiv.innerHTML = detailsInfo
+        details.appendChild(detailsDiv)
+    })
 }
